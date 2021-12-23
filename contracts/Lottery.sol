039294974e8ad40a22d278d6efd0f9e05a316e13
shubhamghimire <shubhamghimire1996@gmail.com>
2021-12-23 22:17:28 +0900

    chainlink price feed added

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,9 +1,26 @@
 pragma solidity ^0.6.6;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery {
-    function enter() public {}
+    address payable[] public players;
+    uint259 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
 
-    function getEntranceFee() public {}
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+    }
+
+    function enter() public payable {
+        // 50$ minimum
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        //
+    }
 
     function startLottery() public {}
+
+    function endLottery() public {}
 }
