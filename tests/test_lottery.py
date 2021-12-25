c1a254bce45fc701a0266bf7de5d423920b8c881
shubhamghimire <shubhamghimire1996@gmail.com>
2021-12-25 13:50:09 +0900

    Entrance fee calculation test code added

---
+++ tests/test_lottery.py
@@ -0,0 +1,14 @@
+# 0.012
+# 120000000000000000
+from brownie import Lottery, accounts
+from web3 import Web3
+
+
+def test_get_entrance_fee():
+    account = accounts[0]
+    lottery = Lottery.deploy(
+        config["networks"][network.show_active()]["eth_usd_price_feed"],
+        {"from": account},
+    )
+    assert lottery.getEntranceFee() > Web3.toWei(0.011, "ether")
+    assert lottery.getEntranceFee() < Web3.toWei(0.015, "ether")
