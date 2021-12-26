bf475fc9c9c2ea37a551c5abcc4aeaa8e9c8181b
shubhamghimire <shubhamghimire1996@gmail.com>
2021-12-26 22:05:01 +0900

    Openzepplin access control mechanism addded

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,19 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.6;
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "openzeppelin/contracts/access/Ownable.sol";
 
 contract Lottery {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATED_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
+
+    // 0
+    // 1
+    // 2
 
     constructor(address _priceFeedAddress) public {
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
     }
 
     function enter() public payable {
         // 50$ minimum
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
         players.push(msg.sender);
     }
 
@@ -27,7 +41,13 @@ contract Lottery {
         return costToEnter;
     }
 
-    function startLottery() public {}
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Can't start a new lottery yet!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
 
     function endLottery() public {}
 }
