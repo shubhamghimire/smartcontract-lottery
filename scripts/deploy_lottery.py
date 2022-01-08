816ecbf9b8dbceb8f245eaa67ad532c08abb4621
shubhamghimire <shubhamghimire1996@gmail.com>
2022-01-08 23:14:06 +0900

    get_contract() function on helpful_scripts added

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -1,5 +1,10 @@
+from scripts.helpful_scripts import get_account, get_contract
+from brownie import Lottery
+
+
 def deploy_lottery():
     account = get_account()
+    lottery = Lottery.deploy(get_contract("eth_usd_price_feed"))
 
 
 def main():
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -19,3 +19,23 @@ def get_account(index=None, id=None):
         return accounts[0]
 
     return accounts.add(config["wallets"]["from_key"])
+
+
+contract_to_mock = {"eth_usd_price_feed": MockV3Aggregator}
+
+
+def get_contract(contract_name):
+    """This function will grab the contract addressed from the brownie config
+    if defined, otherwise it will deploy a mock version of that contract, and
+    return that mock contract.
+
+        Args:
+            contract_name (string)
+
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently deployed
+            version of this contract.
+
+    """
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
