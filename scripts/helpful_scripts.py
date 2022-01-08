c8245022dad69c2f413b2523fd69097bcb70ae84
shubhamghimire <shubhamghimire1996@gmail.com>
2022-01-08 22:58:18 +0900

    help_scripts.py get_account() function refactored

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -4,11 +4,18 @@ FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 
-def get_account():
+def get_account(index=None, id=None):
+    # accounts[0]
+    # accounts.add("env")
+    # accounts.load("id")
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
     if (
         network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
         or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
     ):
         return accounts[0]
-    else:
-        return accounts.add(config["wallets"]["from_key"])
+
+    return accounts.add(config["wallets"]["from_key"])
