41c1ac857cdcd2ec4a68b42f3da6c7f8b9794499
shubhamghimire <shubhamghimire1996@gmail.com>
2022-01-13 21:52:46 +0900

    test_lottery_unit.py unit test script created

---
+++ tests/test_lottery_unit.py
@@ -0,0 +1,15 @@
+from brownie import Lottery, accounts, config, network
+from scripts.deploy_lottery import deploy_lottery
+from web3 import Web3
+
+
+def test_get_entrance_fee():
+    # Arrange
+    lottery = deploy_lottery()
+    # Act
+    # 2000 eth / usd
+    # 2000/1 == 50/x == 0.025
+    expected_entrance_fee = Web3.toWei(0.025, "ether")
+    entrance_fee = lottery.getEntranceFee()
+    # Assert
+    assert expected_entrance_fee == entrance_fee
