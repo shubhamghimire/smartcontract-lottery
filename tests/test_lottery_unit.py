8721f910b76438a5dda8708be3695c38e1751a86
shubhamghimire <shubhamghimire1996@gmail.com>
2022-01-15 13:28:58 +0900

    pytest.skip added

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -1,9 +1,13 @@
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from brownie import Lottery, accounts, config, network
 from scripts.deploy_lottery import deploy_lottery
 from web3 import Web3
+import pytest
 
 
 def test_get_entrance_fee():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
     # Arrange
     lottery = deploy_lottery()
     # Act
