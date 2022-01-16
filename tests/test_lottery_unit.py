24a3b1ac60b0b5c983fdf1ab0646d1f1b7a6da50
shubhamghimire <shubhamghimire1996@gmail.com>
2022-01-16 21:26:27 +0900

    test_can_pick_winner_correctly test completed and tested

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -2,6 +2,7 @@ from scripts.helpful_scripts import (
     LOCAL_BLOCKCHAIN_ENVIRONMENTS,
     get_account,
     fund_with_link,
+    get_contract,
 )
 from brownie import Lottery, accounts, config, network, exceptions
 from scripts.deploy_lottery import deploy_lottery
@@ -72,3 +73,15 @@ def test_can_pick_winner_correctly():
     lottery.enter({"from": get_account(index=1), "value": lottery.getEntranceFee()})
     lottery.enter({"from": get_account(index=2), "value": lottery.getEntranceFee()})
     fund_with_link(lottery)
+    transaction = lottery.endLottery({"from": account})
+    request_id = transaction.events["RequestRandomness"]["requestId"]
+    STATIC_RNG = 777
+    get_contract("vrf_coordinator").callBackWithRandomness(
+        request_id, STATIC_RNG, lottery.address, {"from": account}
+    )
+    starting_balance_of_account = account.balance()
+    balance_of_lottery = lottery.balance()
+    # 777 % 3 = 0
+    assert lottery.recentWinner() == account
+    assert lottery.balance() == 0
+    assert account.balance() == starting_balance_of_account + balance_of_lottery
